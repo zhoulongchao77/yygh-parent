d02268385c493f33708532a18d74679790311258
leifengyang <534096094@qq.com>
2021-09-26 16:24:38 +0800

    修改生产deploy.yml

--- server-gateway/src/main/java/com/atguigu/yygh/ServerGatewayApplication.java
+++ server-gateway/src/main/java/com/atguigu/yygh/ServerGatewayApplication.java
@@ -3,6 +3,7 @@ package com.atguigu.yygh;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class ServerGatewayApplication {
 
